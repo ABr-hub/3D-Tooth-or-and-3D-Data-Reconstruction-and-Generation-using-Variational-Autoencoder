2ecd11f64284bae98637eadf63b6aa159c96b304
ABr-hub <67805783+ABr-hub@users.noreply.github.com>
2024-10-07 11:40:50 +0200

    Add files via upload

---
+++ src/beta_vae.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: ABr_hub
+# @Date:   2024-10-07 11:40:23
+# @Last Modified by:   ABr_hub
+# @Last Modified time: 2024-10-07 11:40:23
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
